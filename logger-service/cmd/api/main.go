b920d6d61d524cf51371b08257a91ede9455235b
Jacob Woodruff <jkwoodruff93@gmail.com>
2023-06-18 15:55:42 -0500

    adding logger-service

---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+const (
+	webPort = "80"
+	rpcPort = "5001"
+	mongoURL = "mongodb://mongo:27017"
+	gRpcPort = "50001"
+)
+
+var client *mongo.Client
+
+
+
+func main() {
+
+}
